89379a088ab71aa5ce7e1d99e82b6a959a1dc1f0
Fantomas <1234556789>
2021-02-05 15:34:50 +0200

    Add Image Picker Controller

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -18,7 +18,20 @@ class NewPlaceViewController: UITableViewController {
     //MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(sourse: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(sourse: .photoLibrary)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             
+            actionSheet.addAction(photo)
+            actionSheet.addAction(camera)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true, completion: nil)
         } else {
             view.endEditing(true)
         }
@@ -28,8 +41,22 @@ class NewPlaceViewController: UITableViewController {
 //MARK: - Text field delegate
 
 extension NewPlaceViewController: UITextFieldDelegate {
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+//MARK: - Work with image
+extension NewPlaceViewController {
+    
+    func chooseImagePicker(sourse: UIImagePickerController.SourceType){
+        if UIImagePickerController.isSourceTypeAvailable(sourse) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = sourse
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
 }
